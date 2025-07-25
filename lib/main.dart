a3763c2ac5ffea96d3aa1a09fc6eaf83e2182c1a
Mayank <patilmayank3238@gmail.com>
2025-07-25 08:47:09 +0200

    BVG Transport Connections App: - Initial commit with flutter project template. - Added all the required libraries and assets initially. - Updated gitIgnore

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+// main.dart
+import 'package:bvg_transport_app/views/transport_info_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const BVGApp());
+
+class BVGApp extends StatelessWidget {
+  const BVGApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BVG Departures',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const TransportInfoPage(),
+    );
+  }
+}
